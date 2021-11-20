3610b4bdb4d35b71cf6fd49119f7497bde4c95d5
Tian Menghan <1416186252@qq.com>
2021-11-20 15:59:04 +0800

    create a AcApp on AcWing

--- acapp/settings.py
+++ acapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h9g_nub#ppqin0q6khi+s4z-+dadd=6px@u!1%y$e)y)zdtrtl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["182.92.188.194"]
+ALLOWED_HOSTS = ["182.92.188.194", "app480.acapp.acwing.com.cn"]
 
 
 # Application definition
